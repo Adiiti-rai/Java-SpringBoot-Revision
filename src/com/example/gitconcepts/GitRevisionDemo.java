68d379f30d14d18e4371b3e9759eb7cd972ba2cf
Aditi Rai <aditi_rai@epam.com>
2026-04-02 08:14:08 +0530

    I have revisied git concepts

---
+++ src/com/example/gitconcepts/GitRevisionDemo.java
@@ -0,0 +1,7 @@
+package com.example.gitconcepts;
+
+public class GitRevisionDemo {
+    public static void main(String[] args){
+        System.out.println("This is the  git Revision demo");
+    }
+}
