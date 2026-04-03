05c5b04632d23358d5920f7bc17b128a9a1e8d7b
Aditi Rai <aditi_rai@epam.com>
2026-04-03 10:48:34 +0530

    I have added 5 stream questions

--- src/com/example/streamsquestions/StreamExamples.java
+++ src/com/example/streamsquestions/StreamExamples.java
@@ -35,5 +35,39 @@ public class StreamExamples {
                 .mapToInt(Integer::intValue)
                 .sum();
         System.out.println("print the sum of all digit in the list:"+sum);
+
+        // find the product of all elements
+        long product= list.stream()
+                .reduce(1,(a,b)->a*b);
+        System.out.println("print the product of all elements in the list:"+product);
+
+        // print the avg of all elements in the list
+        double avg= list.stream()
+                .mapToInt(Integer::intValue)
+                .average()
+                .orElse(0);
+        System.out.println("print the avg of all elements in the list:"+avg);
+
+        //Count the Number of Elements in a List
+        Long count= list.stream()
+                .count();
+        System.out.println("print the count of all elements in the list:"+count);
+
+        // Check if a List Contains a Specific Element
+        boolean result2= list.stream()
+                .anyMatch(x->x==3);
+        System.out.println("print the result of anyMatch:"+result2);
+
+        // Find the First Element in a List
+        int firstElement= list.stream()
+                .findFirst()
+                .orElse(0);
+        System.out.println("print the first element in the list:"+firstElement);
+
+        //Find the last Element in a List
+        int lastElement= list.stream()
+                .reduce((a,b)->b)
+                        .orElse(0);
+        System.out.println("print the last element in the list:"+lastElement);
     }
 }
