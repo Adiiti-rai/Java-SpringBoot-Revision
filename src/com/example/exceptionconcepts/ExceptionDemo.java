f5d98107bc94b5143fa5000e645db1528a2b0f01
Aditi Rai <aditi_rai@epam.com>
2026-04-06 11:14:06 +0530

    write the concepts of exception handling

---
+++ src/com/example/exceptionconcepts/ExceptionDemo.java
@@ -0,0 +1,31 @@
+package com.example.exceptionconcepts;
+
+// What is exception?
+//what is exception handling?
+//why we need exception and exception handling?
+//what are the advantages of exception handling?
+//what are the keywords used in exception handling?
+//what is the error.
+//what is types of error?
+//what is the difference between error and exception?
+//what are the types of exception?
+//1.Checked exception
+//2.Unchecked exception
+//what is the difference between checked and unchecked exception?
+// Custom exception
+//Understand Exception heirarchy
+//what is try and catch block?
+// what is multiple catch block?
+//what is finally block?
+//what is throw and throws keyword?
+//what is the difference between throw and throws keyword?
+//what is nested try-catch block?
+
+
+
+
+public class ExceptionDemo {
+    public static void main(String[] args){
+
+    }
+}
